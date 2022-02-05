b98d479884d987ca2221aa887e218362c4052adb
Tom Adams <thomasjeadams@btinternet.com>
2022-02-05 00:22:53 +0000

    added a button to clear localstorage

--- src/components/DayInfo.js
+++ src/components/DayInfo.js
@@ -2,40 +2,41 @@ import React, { useState } from 'react';
 import useLocalStorage from '../hooks/useLocalStorage';
 
 export default function DayInfo({i}) {
-    const [formData, setFormData] = useState({
-        location: '',
-        language: '',
-        accomodation: 'Hotel'
-    })
-    const [dayData, setDayData] = useLocalStorage(`day-${i +1}`,{})
+  const [dayData, setDayData] = useLocalStorage(`day-${i +1}`,{})
+  const [formData, setFormData] = useState({
+      location: dayData.location,
+      language: dayData.language,
+      accomodation: dayData.accomodation || 'Hotel'
+  })
 
-    function handleInput(e) {
-        console.log("e: ",e)
-        setFormData({...formData, [e.target.name]: e.target.value})
-        console.log("formData: ", formData)
-    }
+  function handleInput(e) {
+      console.log("e: ",e)
+      setFormData({...formData, [e.target.name]: e.target.value})
+      console.log("formData: ", formData)
+  }
 
-    function handleLocationSubmit(e) {
-        e.preventDefault()
-        setDayData(formData)
-    }
+  function handleLocationSubmit(e) {
+      e.preventDefault()
+      setDayData(formData)
+  }
   return(
       <div className='day-info'>
         <h3>Day {`${i + 1}`}</h3>
-        
-        <form onSubmit={handleLocationSubmit} className="day-info-form">
-          <label for='location'>Location:</label>
-          <input type='text' name='location' onChange={handleInput} />
-          <label for='language'>Language:</label>
-          <input type='text' name='language' onChange={handleInput} />
-          <label for='accomodation'>Accomodation:</label>
-          <select name='accomodation' onChange={handleInput}>
-            <option value='Hotel'>Hotel</option>
-            <option value='AirBnB'>AirBnB</option>
-            <option value='Hostel'>Hostel</option>
-          </select>
-          <button type='submit'>Set</button>
-        </form>
+        <div className='day-info-form-container'>
+          <form onSubmit={handleLocationSubmit} className='day-info-form-content'>
+            <label htmlFor='location'>Location:</label>
+            <input type='text' name='location' value={formData.location} onChange={handleInput} />
+            <label htmlFor='language'>Language:</label>
+            <input type='text' name='language' value={formData.language} onChange={handleInput} />
+            <label htmlFor='accomodation'>Accomodation:</label>
+            <select name='accomodation' onChange={handleInput}>
+              {dayData.accomodation === 'Hotel' ? <option value='Hotel' selected>Hotel</option> : <option value='Hotel'>Hotel</option>}
+              {dayData.accomodation === 'AirBnB' ? <option value='AirBnB' selected>AirBnB</option> : <option value='AirBnB'>AirBnB</option>}
+              {dayData.accomodation === 'Hostel' ? <option value='Hostel' selected>Hostel</option> : <option value='Hostel'>Hostel</option>}
+            </select>
+            <button type='submit'>Set</button>
+          </form>
+        </div>
 
         <p>Location: {dayData.location}</p>
         <p>Language: {dayData.language}</p>
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -2,11 +2,23 @@ import React, { useState } from 'react';
 import DayListItem from './DayListItem';
 
 export default function SideBar({selectedDay, setSelectedDay, tripLength, setTripLength}) {
-    
+  const [numberInputed, setNumberInputed] = useState(tripLength)
+
+  function handleChange(e) {
+    e.preventDefault()
+    setNumberInputed(e.target.value)
+  }
 
     function handleTripLengthSubmit(e) {
         e.preventDefault()
         setTripLength(e.target[0].value)
+    }
+
+    function clearLocalStorage() {
+      for (let i = 1; i <= tripLength; i++) {
+        localStorage.removeItem(`travel-planner-day-${i}`)
+        localStorage.removeItem(`travel-planner-trip-length`)
+      }
     }
 
     const all_days = []
@@ -18,14 +30,14 @@ export default function SideBar({selectedDay, setSelectedDay, tripLength, setTri
       <div className='sidebar'>
       <h1 className='trip-title'>Your trip</h1>
       <form onSubmit={handleTripLengthSubmit}>
-        <input type='number' />
+        <input type='number' value={numberInputed} onChange={handleChange}/>
         <button type='submit'>Set</button>
       </form>
       <p>Trip length {tripLength}</p>
       <ul className='trip-days'>
         {all_days}
       </ul>
+      <button onClick={clearLocalStorage}>Clear Trip</button>
       </div>
-
   );
 }
